360d6594a5d507adc6d71562b6aa5d0ae823a2af
Johnny John <incognitosxy@gmail.com>
2016-10-03 04:34:29 -0300

    Adição de Comentários

--- config.php
+++ config.php
@@ -8,17 +8,22 @@
 * Author URI: http://www.finer.com.br/
 **/
 
+// Definição de constantes
 define('PLUGIN_PATH',   plugin_dir_path(__FILE__));
 define('PLUGIN_URL',    plugins_url('', __FILE__));
 
+
+// Dependências do plugin
 require_once('inc/functions.php');
 require_once('porcentagem.php');
 require_once('custom_meta.php');
 
 
+// Instanciamos a classe principal e em seguida dos custom meta box
 $porcentagem = new Porcentagem();
 $meta 		 = new CustomMeta();
 
+// Invocamos o método de inicialização do Plugin
 $porcentagem->init();
 
 
